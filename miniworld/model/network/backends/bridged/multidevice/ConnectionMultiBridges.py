e3aa00c6eaf7887c700aa5be437d5ea024435305
Nils Schmidt <nightm4re@freenet.de>
2017-03-26 18:59:16 +0200

    Removed commented code

--- miniworld/model/network/backends/bridged/multidevice/ConnectionMultiBridges.py
+++ miniworld/model/network/backends/bridged/multidevice/ConnectionMultiBridges.py
@@ -1,6 +1,5 @@
 from miniworld import singletons
 from miniworld.model.network.backends.bridged.Connection import ConnectionDummy
-from miniworld.model.network.linkqualitymodels.LinkQualityConstants import *
 
 def ConnectionMultiBridges():
     class ConnectionMultiBridges(ConnectionDummy()):
@@ -9,7 +8,6 @@ def ConnectionMultiBridges():
         ### Superclass stuff
         #########################################
 
-        # TODO: #84 move to NetworkBackendBridgedMultiDevice
         def tap_link_up(self, tap_x, tap_y, up=True):
 
             # use internal :py:class:`.connection_book_keeper` to only change the NIC state if necessary
@@ -31,34 +29,5 @@ def ConnectionMultiBridges():
 
         def _get_default_class(self):
             return 1
-
-        # def shape_device(self, dev_name, connection_id, link_quality_dict):
-        #     '''
-        #     Parameters
-        #     ----------
-        #     dev_name : str
-        #     connection_id : str
-        #     rate : int
-        #
-        #     tc qdisc add dev $DEV root handle 1:0 htb default 12
-        #     tc class add dev $DEV parent 1:0 classid 1:1 htb rate 190kbit ceil 190kbit
-        #     tc class add dev $DEV parent 1:1 classid 1:12 htb rate 100kbit ceil 190kbit prio 2
-        #     '''
-        #
-        #     rate = link_quality_dict.get(LINK_QUALITY_KEY_BANDWIDTH)
-        #     delay = link_quality_dict.get(LINK_QUALITY_KEY_DELAY)
-        #
-        #     if rate is not None:
-        #
-        #         # if singletons.simulation_manager.current_step == 0:
-        #         # add root
-        #         self.add_shell_command(self.EVENT_LINK_SHAPE_ADD_QDISC,
-        #                                # TODO: ADD/REMOVE default 1
-        #                                "tc qdisc replace dev {} root handle 1:0 htb default 1".format(dev_name))
-        #
-        #         # add first and only class, use htb shaping algorithm
-        #         self.add_shell_command(self.EVENT_LINK_SHAPE_ADD_CLASS,
-        #                                "tc class replace dev {} parent 1:0 classid 1:{id} htb rate {rate}kbit".format(
-        #                                    dev_name, rate=rate, id=connection_id))
 
     return ConnectionMultiBridges
