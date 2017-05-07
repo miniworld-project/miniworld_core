12a5a86567c25815bc7f64c88f7a04c4157f205c
Nils Schmidt <nightm4re@freenet.de>
2017-05-07 12:41:46 +0200

    Fetch example images via http to save git bfs bandwidth

---
+++ examples/get_images.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+wget https://media.githubusercontent.com/media/miniworld-project/miniworld_core/nightly/examples/openwrt-15.05-x86-kvm_guest-combined-ext4_batman_adv.img
+wget https://media.githubusercontent.com/media/miniworld-project/miniworld_core/nightly/examples/openwrt-15.05.1-x86-kvm_guest-combined-ext4.img
