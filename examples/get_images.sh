e446b61ad9b28d65a1d2773d1b48aa0ff733056d
Nils Schmidt <nightm4re@freenet.de>
2017-05-12 08:53:15 +0200

    Download examples images for tests

--- examples/get_images.sh
+++ examples/get_images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-wget https://media.githubusercontent.com/media/miniworld-project/miniworld_core/nightly/examples/openwrt-15.05-x86-kvm_guest-combined-ext4_batman_adv.img
-wget https://media.githubusercontent.com/media/miniworld-project/miniworld_core/nightly/examples/openwrt-15.05.1-x86-kvm_guest-combined-ext4.img
+wget -c https://www.dropbox.com/s/39adlk1alwovsq7/openwrt-15.05.1-x86-kvm_guest-combined-ext4.img?dl=0 -O openwrt-15.05.1-x86-kvm_guest-combined-ext4.img.1
+wget -c https://www.dropbox.com/s/b2xx8yim1p2xb0l/openwrt-15.05-x86-kvm_guest-combined-ext4_batman_adv.img?dl=0 -O openwrt-15.05-x86-kvm_guest-combined-ext4_batman_adv.img.1
--- tests/acceptance/test_examples.py
+++ tests/acceptance/test_examples.py
@@ -1,35 +1,47 @@
+import subprocess
+
+import pytest
 from typing import Dict
 
 from miniworld.util import JSONConfig
 
 
-# TODO: examples/batman_adv.json, problem is configurator
-def test_snapshot_boot_single_scenario(image_path, runner):
-    with runner() as r:
-        for _ in range(5):
-            scenario = JSONConfig.read_json_config('examples/nb_bridged_lan.json')  # type: Dict
+@pytest.fixture(scope='session')
+def download_examples():
+    subprocess.check_call(['./get_images.sh'], cwd='examples/')
+
+
+@pytest.mark.usefixtures('download_examples')
+class TestExamples:
+    # TODO: examples/batman_adv.json, problem is configurator
+
+    def test_snapshot_boot_single_scenario(self, runner):
+        with runner() as r:
+            for _ in range(5):
+                scenario = JSONConfig.read_json_config('examples/nb_bridged_lan.json')  # type: Dict
+                r.start_scenario(scenario)
+                r.step()
+                r.step()
+                r.stop(hard=False)
+
+    # TODO: test stop/step
+    @pytest.mark.usefixtures('download_examples')
+    def test_snapshot_boot_multiple_scenarios(self, runner):
+        with runner() as r:
+            scenario = JSONConfig.read_json_config('examples/batman_adv.json')  # type: Dict
             r.start_scenario(scenario)
-            r.step()
-            r.step()
+            for _ in range(5):
+                r.step()
             r.stop(hard=False)
 
-# TODO: test stop/step
-def test_snapshot_boot_multiple_scenarios(image_path, runner):
-    with runner() as r:
-        scenario = JSONConfig.read_json_config('examples/batman_adv.json')  # type: Dict
-        r.start_scenario(scenario)
-        for _ in range(5):
-            r.step()
-        r.stop(hard=False)
+            scenario = JSONConfig.read_json_config('examples/nb_bridged_lan.json')  # type: Dict
+            r.start_scenario(scenario)
+            for _ in range(5):
+                r.step()
+            r.stop(hard=False)
 
-        scenario = JSONConfig.read_json_config('examples/nb_bridged_lan.json')  # type: Dict
-        r.start_scenario(scenario)
-        for _ in range(5):
-            r.step()
-        r.stop(hard=False)
-
-        scenario = JSONConfig.read_json_config('examples/nb_bridged_wifi.json')  # type: Dict
-        r.start_scenario(scenario)
-        for _ in range(5):
-            r.step()
-        r.stop(hard=False)
+            scenario = JSONConfig.read_json_config('examples/nb_bridged_wifi.json')  # type: Dict
+            r.start_scenario(scenario)
+            for _ in range(5):
+                r.step()
+            r.stop(hard=False)
