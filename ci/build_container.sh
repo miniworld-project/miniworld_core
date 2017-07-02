f0d9ff6dc884e3e05225c668aafde86597a7e159
Nils Schmidt <nightm4re@freenet.de>
2017-07-02 18:58:34 +0200

    Use master branch instead of nightly in files

--- ci/build_container.sh
+++ ci/build_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo $BRANCH
-docker build --pull -t miniworldproject/miniworld_core:${BRANCH-nightly} -f ci/Dockerfile $@ --build-arg BRANCH=$BRANCH .
+docker build --pull -t miniworldproject/miniworld_core:${BRANCH-master} -f ci/Dockerfile $@ --build-arg BRANCH=${BRANCH-master} .
